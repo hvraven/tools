e668ca41495e695e2db9e5fc0cab5f185d7d391b
Hendrik v. Raven <h.v.raven@googlemail.com>
2011-11-14 14:39:18 +0100

    added boost program options

--- fls/fls.cpp
+++ fls/fls.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <boost/filesystem.hpp>
+#include <boost/program_options.hpp>
 
-using namespace boost
+using namespace boost;
 
 void list_content( filesystem::path p )
 {
@@ -16,25 +17,57 @@ void list_content( filesystem::path p )
 
 int main(int argc, char* argv[])
 {
-  if (argc < 2)
-  {
-    std::cout << "Usage: fls path\n";
-    return 1;
-  }
-
-  filesystem::path p = argv[1];
-
-  if ( filesystem::is_regular_file( p ) )
+  try
     {
-      std::cout << p << " "
-		<< filesystem::file_size(p) << '\n';
-      return 0;
+      program_options::options_description generic("Generic Options:");
+      generic.add_options()
+	("help,h", "print this message");
+
+      program_options::options_description hidden("Hidden Options");
+      hidden.add_options()
+	("file", program_options::value<std::string>(), "file to stat");
+
+      program_options::options_description cmdline_options;
+      cmdline_options.add(generic).add(hidden);
+
+      program_options::options_description visible("Supported options");
+      visible.add(generic);
+
+      program_options::positional_options_description posdesc;
+      posdesc.add("file", -1);
+
+      program_options::variables_map vm;
+      program_options::store(program_options::command_line_parser(argc, argv).
+			     options(cmdline_options).positional(posdesc).run(),
+			     vm);
+      program_options::notify(vm);
+
+      if (vm.count("help")) {
+	std::cout << visible << std::endl;
+	return 1;
+      }
+
+      std::cout << "File: " << vm["file"].as<std::string>() << std::endl;
+
+      filesystem::path p = vm["file"].as<std::string>();
+
+      if ( filesystem::is_regular_file( p ) )
+	{
+	  std::cout << p << " "
+		    << filesystem::file_size(p) << '\n';
+	  return 0;
+	}
+      else if ( filesystem::is_directory(p) )
+	{
+	  list_content(p);
+	  return 0;
+	}
+      else
+	return 1;
     }
-  else if ( filesystem::is_directory(p) )
+  catch( std::exception& e )
     {
-      list_content(p);
-      return 0;
-    }
-    else
+      std::cerr << e.what() << std::endl;
       return 1;
+    }
 }
