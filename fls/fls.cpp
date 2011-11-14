455b55c45aa0effd91e857f032a2e7a5f4bfe324
Hendrik v. Raven <h.v.raven@googlemail.com>
2011-11-14 08:39:07 +0100

    changed namespace again (to using boost)

--- fls/fls.cpp
+++ fls/fls.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 #include <boost/filesystem.hpp>
 
-void list_content( boost::filesystem::path p )
+using namespace boost
+
+void list_content( filesystem::path p )
 {
-  for ( boost::filesystem::directory_iterator dir(p);
-	dir != boost::filesystem::directory_iterator(); dir++ )
+  for ( filesystem::directory_iterator dir(p);
+	dir != filesystem::directory_iterator(); dir++ )
     {
       std::cout << *dir << std::endl;
-      if ( boost::filesystem::is_directory(*dir) )
+      if ( filesystem::is_directory(*dir) )
 	list_content( *dir );
     }
 }
@@ -16,19 +18,19 @@ int main(int argc, char* argv[])
 {
   if (argc < 2)
   {
-    std::cout << "Recursive ls\nUsage: fls path\n";
+    std::cout << "Usage: fls path\n";
     return 1;
   }
 
-  boost::filesystem::path p = argv[1];
+  filesystem::path p = argv[1];
 
-  if ( boost::filesystem::is_regular_file( p ) )
+  if ( filesystem::is_regular_file( p ) )
     {
       std::cout << p << " "
-		<< boost::filesystem::file_size(p) << '\n';
+		<< filesystem::file_size(p) << '\n';
       return 0;
     }
-  else if ( boost::filesystem::is_directory(p) )
+  else if ( filesystem::is_directory(p) )
     {
       list_content(p);
       return 0;
